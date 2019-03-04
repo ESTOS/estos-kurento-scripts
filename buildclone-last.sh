a5697c7b9f8218689d391bdae3e49103d2d1d5c6
Rolf Burkhardt <rolf.burkhardt@estos.de>
2019-03-04 11:02:36 +0100

    RTCSP-729 prepare versions and update - buildclone-last.sh is a script to checkout the last release build - can be taged here - buildlast.sh checks out the last build and builds it again - build63.sh now uses buildversion.sh with his buildclone-estos6.3.sh - this commit was a build with updated gstreamer (estos6.3) and old kurento-modules (estos6.2)

---
+++ buildclone-last.sh
@@ -0,0 +1,76 @@
+git clone https://github.com/ESTOS/kms-cmake-utils.git
+cd kms-cmake-utils
+git checkout 42fdefa5ebb7069954e6dee8989f2c2b939c5428
+cd ..
+git clone https://github.com/ESTOS/kurento-module-creator.git
+cd kurento-module-creator
+git checkout 0bdbd17d1b9fa85133403187698cad6223ac6eb6
+cd ..
+git clone https://github.com/ESTOS/gstreamer.git
+cd gstreamer
+git checkout c5dd87a593feac9244152c83b7632043d461625f
+cd ..
+git clone https://github.com/ESTOS/gst-plugins-base.git
+cd gst-plugins-base
+git checkout 7bf50f79f694dda7676db8364d7c50bb2fef1c2b
+cd ..
+git clone https://github.com/ESTOS/jsoncpp.git
+cd jsoncpp
+git checkout c03c9e02f2946342138c6a274b0e0d3fb20d399c
+cd ..
+git clone https://github.com/ESTOS/kms-jsonrpc.git
+cd kms-jsonrpc
+git checkout 265d958539638b8aee2464752ec74dcc62f48d8b
+cd ..
+git clone https://github.com/ESTOS/libvpx.git
+cd libvpx
+git checkout cbecf57f3e0d85a7b7f97f3ab7c507f6fe640a93
+cd ..
+git clone https://github.com/ESTOS/kms-core.git
+cd kms-core
+git checkout 760cde34781afad8627761993cd021ee90b26432
+cd ..
+git clone https://github.com/ESTOS/libevent.git
+cd libevent
+git checkout ba78ba9e8ba4c964dd5d14a281d7421c95d37937
+cd ..
+git clone https://github.com/ESTOS/kurento-media-server.git
+cd kurento-media-server
+git checkout 14af714a409554d638bad23e2eb7459e30bc0427
+cd ..
+git clone https://github.com/ESTOS/usrsctp.git
+cd usrsctp
+git checkout e5c5d352b624333e606288ccf323aad8e38c9039
+cd ..
+git clone https://github.com/ESTOS/openwebrtc-gst-plugins.git
+cd openwebrtc-gst-plugins
+git checkout 079ccd07956a33c8c5bcca1c1a39cc19b8167370
+cd ..
+git clone https://github.com/ESTOS/libnice.git
+cd libnice
+git checkout 377636d8224a676ea008a4cbc43dfa0d395a0ef6
+cd ..
+git clone https://github.com/ESTOS/kms-elements.git
+cd kms-elements
+git checkout 484d8e5b6f2e41d39728b5f126cddcd5b640d928
+cd ..
+git clone https://github.com/ESTOS/opencv.git
+cd opencv
+git checkout d68e3502278d6fc5a1de0ce8f7951d9961b20913
+cd ..
+git clone https://github.com/ESTOS/kms-filters.git
+cd kms-filters
+git checkout 0f091d5beac6b1e15ead59ff9e563843665f8644
+cd ..
+git clone https://github.com/ESTOS/gst-plugins-good.git
+cd gst-plugins-good
+git checkout d3d7a237f671ebf64a09e39ac497bf5df534624f
+cd ..
+git clone https://github.com/ESTOS/libsrtp.git
+cd libsrtp
+git checkout 078c4a5b2a4380dea025998ceea9e0a1e3164d2d
+cd ..
+git clone https://github.com/ESTOS/gst-plugins-bad.git
+cd gst-plugins-bad
+git checkout fb149e42687a098cf4cf233cccbedf35f720cf91
+cd ..
---
+++ buildlast.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# used to build the last version based on buildclone-last.sh
+./buildversion.sh buildclone-last.sh
