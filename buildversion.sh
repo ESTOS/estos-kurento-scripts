6edf0ba1a343b755d12645f37d9a9ead3489941d
Rolf Burkhardt <rolf.burkhardt@estos.de>
2019-03-12 12:29:09 +0100

    RTCSP-729 prepare versions and update - remove tabs on module build opencv

--- buildversion.sh
+++ buildversion.sh
@@ -154,47 +154,47 @@ pause
 sudo mingw32-make install
 cd ..
 
-echo "3.15 opencv"		
-cd opencv/		
-mkdir ../opencv-build		
-cd ../opencv-build/		
-mingw32-cmake \		
-  -DBUILD_PERF_TESTS=false \		
-  -DBUILD_TESTS=false \		
-  -DWITH_DSHOW=false \		
-  -DWITH_WIN32UI=false \		
-  -DWITH_OPENCL=false \		
-  -DWITH_VFW=false \		
-  -DBUILD_ZLIB=false \		
-  -DBUILD_TIFF=false \		
-  -DBUILD_JASPER=false \		
-  -DBUILD_JPEG=false \		
-  -DBUILD_PNG=false \		
-  -DBUILD_OPENEXR=false \		
-  -DWITH_FFMPEG=false \		
-  -DBUILD_opencv_flann=OFF \		
-  -DBUILD_opencv_photo=OFF \		
-  -DBUILD_opencv_video=OFF \		
-  -DBUILD_opencv_ml=OFF \		
-  ../opencv		
-sed -i 's/-isystem\ \/usr\/i686-w64-mingw32\/sys-root\/mingw\/include/ /g' \		
-  modules/core/CMakeFiles/opencv_core.dir/includes_CXX.rsp		
-sed -i 's/-isystem\ \/usr\/i686-w64-mingw32\/sys-root\/mingw\/include\ / /g' ./modules/highgui/CMakeFiles/opencv_highgui.dir/includes_CXX.rsp		
-mingw32-make		
-sudo mingw32-make install		
-sudo cp unix-install/opencv.pc /usr/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig/		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_core2413.dll.a \		
-  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_core.a		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_highgui2413.dll.a \		
-  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_highgui.a		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_imgproc2413.dll.a \		
-  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_imgproc.a		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_objdetect2413.dll.a \		
-  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_objdetect.a		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_core2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_core2413.dll		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_highgui2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_highgui2413.dll		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_imgproc2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_imgproc2413.dll		
-sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_objdetect2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_objdetect2413.dll		
+echo "3.15 opencv"
+cd opencv
+mkdir ../opencv-build
+cd ../opencv-build
+mingw32-cmake \
+  -DBUILD_PERF_TESTS=false \
+  -DBUILD_TESTS=false \
+  -DWITH_DSHOW=false \
+  -DWITH_WIN32UI=false \
+  -DWITH_OPENCL=false \
+  -DWITH_VFW=false \
+  -DBUILD_ZLIB=false \
+  -DBUILD_TIFF=false \
+  -DBUILD_JASPER=false \
+  -DBUILD_JPEG=false \
+  -DBUILD_PNG=false \
+  -DBUILD_OPENEXR=false \
+  -DWITH_FFMPEG=false \
+  -DBUILD_opencv_flann=OFF \
+  -DBUILD_opencv_photo=OFF \
+  -DBUILD_opencv_video=OFF \
+  -DBUILD_opencv_ml=OFF \
+  ../opencv
+sed -i 's/-isystem\ \/usr\/i686-w64-mingw32\/sys-root\/mingw\/include/ /g' \
+  modules/core/CMakeFiles/opencv_core.dir/includes_CXX.rsp
+sed -i 's/-isystem\ \/usr\/i686-w64-mingw32\/sys-root\/mingw\/include\ / /g' ./modules/highgui/CMakeFiles/opencv_highgui.dir/includes_CXX.rsp
+mingw32-make
+sudo mingw32-make install
+sudo cp unix-install/opencv.pc /usr/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig/
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_core2413.dll.a \
+  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_core.a
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_highgui2413.dll.a \
+  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_highgui.a
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_imgproc2413.dll.a \
+  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_imgproc.a
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/lib/libopencv_objdetect2413.dll.a \
+  /usr/i686-w64-mingw32/sys-root/mingw/lib/libopencv_objdetect.a
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_core2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_core2413.dll
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_highgui2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_highgui2413.dll
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_imgproc2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_imgproc2413.dll
+sudo ln -s /usr/i686-w64-mingw32/sys-root/mingw/x86/mingw/bin/libopencv_objdetect2413.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libopencv_objdetect2413.dll
 cd ..
 
 
