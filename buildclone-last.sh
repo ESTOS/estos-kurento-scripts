18681b3be943341880cc44168d834ecd4df8ecd0
Rolf Burkhardt <rolf.burkhardt@estos.de>
2019-03-12 13:56:16 +0100

    Hotfix build - gst-plugins-good: RTCSP-862 Delay on DTMF-Dial - kms-elements: Fix crash if we find an "rtcp" attribute in sdp -> attr->key has "rtcp"

---
+++ buildclone-last.sh
@@ -0,0 +1,78 @@
+git clone https://github.com/ESTOS/kms-cmake-utils.git
+cd kms-cmake-utils
+git checkout 18eb5a9c21c191a2a672500dc553b55a0953f32d
+cd ..
+git clone https://github.com/ESTOS/kurento-module-creator.git
+cd kurento-module-creator
+git checkout 0bdbd17d1b9fa85133403187698cad6223ac6eb6
+cd ..
+git clone https://github.com/ESTOS/gstreamer.git
+cd gstreamer
+git checkout 5be21c6f42eac01f8e2180ab583c6ebfbcbe4993
+cd ..
+git clone https://github.com/ESTOS/gst-plugins-base.git
+cd gst-plugins-base
+git checkout 7b19cfd259967a5c37280a3adf71106edde4e888
+cd ..
+git clone https://github.com/ESTOS/jsoncpp.git
+cd jsoncpp
+git checkout c03c9e02f2946342138c6a274b0e0d3fb20d399c
+cd ..
+git clone https://github.com/ESTOS/kms-jsonrpc.git
+cd kms-jsonrpc
+git checkout 265d958539638b8aee2464752ec74dcc62f48d8b
+cd ..
+git clone https://github.com/ESTOS/libvpx.git
+cd libvpx
+git checkout cbecf57f3e0d85a7b7f97f3ab7c507f6fe640a93
+cd ..
+git clone https://github.com/ESTOS/kms-core.git
+cd kms-core
+git checkout 760cde34781afad8627761993cd021ee90b26432
+cd ..
+git clone https://github.com/ESTOS/libevent.git
+cd libevent
+git checkout ba78ba9e8ba4c964dd5d14a281d7421c95d37937
+cd ..
+git clone https://github.com/ESTOS/kurento-media-server.git
+cd kurento-media-server
+git checkout 14af714a409554d638bad23e2eb7459e30bc0427
+cd ..
+git clone https://github.com/ESTOS/usrsctp.git
+cd usrsctp
+git checkout e5c5d352b624333e606288ccf323aad8e38c9039
+cd ..
+git clone https://github.com/ESTOS/openwebrtc-gst-plugins.git
+cd openwebrtc-gst-plugins
+git checkout 079ccd07956a33c8c5bcca1c1a39cc19b8167370
+cd ..
+git clone https://github.com/ESTOS/libnice.git
+cd libnice
+git checkout 050fbaea22c797249b28737019835319274ca1b8
+cd ..
+git clone https://github.com/ESTOS/kms-elements.git
+cd kms-elements
+#old 6.2 -> 746ac2435b99b4b5b81d7d3d704e594534d85f06
+git checkout 484d8e5b6f2e41d39728b5f126cddcd5b640d928
+cd ..
+git clone https://github.com/ESTOS/opencv.git
+cd opencv
+git checkout d68e3502278d6fc5a1de0ce8f7951d9961b20913
+cd ..
+git clone https://github.com/ESTOS/kms-filters.git
+cd kms-filters
+git checkout 0f091d5beac6b1e15ead59ff9e563843665f8644
+cd ..
+git clone https://github.com/ESTOS/gst-plugins-good.git
+cd gst-plugins-good
+#old 6.2 -> 39d8ed93af0be5a663963ee0150e500f68a3f3b4
+git checkout cada209a81c8bd01f2846e3f70305d0a66df998c
+cd ..
+git clone https://github.com/ESTOS/libsrtp.git
+cd libsrtp
+git checkout 078c4a5b2a4380dea025998ceea9e0a1e3164d2d
+cd ..
+git clone https://github.com/ESTOS/gst-plugins-bad.git
+cd gst-plugins-bad
+git checkout 3db37b1965c00e820babfcd3e8ee7a257198ef56
+cd ..
