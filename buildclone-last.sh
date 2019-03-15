154323a846e285930c6841f1c638c460835d0ba3
Rolf Burkhardt <rolf.burkhardt@estos.de>
2019-03-15 09:14:26 +0100

    RTCSP-729 prepare versions and update - add buildable and runable version

--- buildclone-last.sh
+++ buildclone-last.sh
@@ -1,10 +1,10 @@
 git clone https://github.com/ESTOS/kms-cmake-utils.git
 cd kms-cmake-utils
-git checkout 42fdefa5ebb7069954e6dee8989f2c2b939c5428
+git checkout d408e638eaabffdb09c508813706abcd52c88cbf
 cd ..
 git clone https://github.com/ESTOS/kurento-module-creator.git
 cd kurento-module-creator
-git checkout 0bdbd17d1b9fa85133403187698cad6223ac6eb6
+git checkout 422d3f3f91cd875af93713c1f71037cb6e9e0d85
 cd ..
 git clone https://github.com/ESTOS/gstreamer.git
 cd gstreamer
@@ -16,19 +16,19 @@ git checkout 7bf50f79f694dda7676db8364d7c50bb2fef1c2b
 cd ..
 git clone https://github.com/ESTOS/jsoncpp.git
 cd jsoncpp
-git checkout c03c9e02f2946342138c6a274b0e0d3fb20d399c
+git checkout 79efbfde69a285caca20b494a0f94b0528847088
 cd ..
 git clone https://github.com/ESTOS/kms-jsonrpc.git
 cd kms-jsonrpc
-git checkout 265d958539638b8aee2464752ec74dcc62f48d8b
+git checkout ae5ae3184a41a293eb91e8c8a329dbc12b980411
 cd ..
 git clone https://github.com/ESTOS/libvpx.git
 cd libvpx
-git checkout cbecf57f3e0d85a7b7f97f3ab7c507f6fe640a93
+git checkout a90944ce794986d8c0daab1449903909ba1956a7
 cd ..
 git clone https://github.com/ESTOS/kms-core.git
 cd kms-core
-git checkout 760cde34781afad8627761993cd021ee90b26432
+git checkout 88355da824a241d7abc28e1134c5a4a22f0bc2d4
 cd ..
 git clone https://github.com/ESTOS/libevent.git
 cd libevent
@@ -36,7 +36,7 @@ git checkout ba78ba9e8ba4c964dd5d14a281d7421c95d37937
 cd ..
 git clone https://github.com/ESTOS/kurento-media-server.git
 cd kurento-media-server
-git checkout 14af714a409554d638bad23e2eb7459e30bc0427
+git checkout 6f79fdfad2783b251af572d032f1f51fdc0a11e7
 cd ..
 git clone https://github.com/ESTOS/usrsctp.git
 cd usrsctp
@@ -52,7 +52,7 @@ git checkout 377636d8224a676ea008a4cbc43dfa0d395a0ef6
 cd ..
 git clone https://github.com/ESTOS/kms-elements.git
 cd kms-elements
-git checkout 484d8e5b6f2e41d39728b5f126cddcd5b640d928
+git checkout d4cd46e3c9200a72480c4f7810efc53330a43f78
 cd ..
 git clone https://github.com/ESTOS/opencv.git
 cd opencv
@@ -60,15 +60,15 @@ git checkout d68e3502278d6fc5a1de0ce8f7951d9961b20913
 cd ..
 git clone https://github.com/ESTOS/kms-filters.git
 cd kms-filters
-git checkout 0f091d5beac6b1e15ead59ff9e563843665f8644
+git checkout 9a593d16e0899708101e8e8c1c66df2d7fe1a1cb
 cd ..
 git clone https://github.com/ESTOS/gst-plugins-good.git
 cd gst-plugins-good
-git checkout d3d7a237f671ebf64a09e39ac497bf5df534624f
+git checkout 592b521f7baa2bb4bfce2f39a32520108eef7e7d
 cd ..
 git clone https://github.com/ESTOS/libsrtp.git
 cd libsrtp
-git checkout 078c4a5b2a4380dea025998ceea9e0a1e3164d2d
+git checkout 5ec1baa78cd35b88bfbb2b0600a0f8262f3cf20b
 cd ..
 git clone https://github.com/ESTOS/gst-plugins-bad.git
 cd gst-plugins-bad
