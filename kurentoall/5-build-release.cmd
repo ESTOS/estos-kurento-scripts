64ebad8e8731e1ff808773beda1aa34d31191026
Rolf Burkhardt <rolf.burkhardt@estos.de>
2026-07-02 11:51:05 +0200

    PROCALL-3103 msys-build - 5-build-release.cmd makes all up to emswindows64_*.zip - log in build-release.log and logbuildmain.txt - use x:\dev\tools a7bc6893800bfadc8ad812081cf56f738729d283
    https://github.com/ESTOS/glib.git		2.89.0
    https://github.com/ESTOS/libnice.git		aca0b1fce62e776c88af9dad63bbb3cfccf7dc2f
    https://github.com/ESTOS/gstreamer.git		3447c72ce2556f7992efced1f608da16f2c850e3
    https://github.com/ESTOS/opencv.git		4.13.0
    https://github.com/ESTOS/openssl.git		openssl-3.0.20
    https://github.com/ESTOS/websocketpp.git	37c48feaa6ad6746fd9df68aa674f0377579a705
    https://github.com/ESTOS/kurento.git		8dfad3783b7d368bd3ded15764f6933a272d46ec

--- kurentoall/5-build-release.cmd
+++ kurentoall/5-build-release.cmd
@@ -1,18 +1,23 @@
 rem @echo off
-@echo 
+call :main > build-release.log 2>&1
+goto :eof
+
+:main
 rem git archive --remote="ssh://git@gitlab.estos.de/estos/windows-global/tools.git" --format=zip feature/PROCALL-3103-msys-build mingw/msys64.zip > msys64.zip
 rem get it from remote with a tag or branch
 rem git archive --remote="ssh://git@gitlab.estos.de/estos/windows-global/tools.git" --format=tar a7bc6893 mingw/msys64.zip | tar -xO > msys64.zip
 rem get it from local with a hash
-git -C x:\dev\tools show a7bc6893:mingw/msys64.zip > msys64.zip
+git -C x:\dev\tools show a7bc6893800bfadc8ad812081cf56f738729d283:mingw/msys64.zip > msys64.zip
+if exist msys64 rmdir /s /q msys64
 unzip msys64.zip
 rem x:\dev\tools\mingw\msys64\usr\bin\bash.exe -lc "/x/dev/estos-kurento-scripts/kurentoall/1-buildmain.sh build_kurento"
 rem set BASH=x:\dev\tools\mingw\msys64\usr\bin\bash.exe
-set BASH=x:\dev\estos-kurento-scripts\kurentoall\mingw\msys64\usr\bin\bash.exe
+set BASH=x:\dev\estos-kurento-scripts\kurentoall\msys64\usr\bin\bash.exe
 set CHERE_INVOKING=1
 set MSYSTEM=MINGW64
 set SCRIPTPATH=/x/dev/estos-kurento-scripts/kurentoall
 
+rem %BASH% -lc "ls -la"
 %BASH% -lc "%SCRIPTPATH%/1-buildmain.sh buildalllog"
 %BASH% -lc "%SCRIPTPATH%/2.0-buildcopykmswindows.sh"
 %BASH% -lc "%SCRIPTPATH%/2.1-builddist.sh"
